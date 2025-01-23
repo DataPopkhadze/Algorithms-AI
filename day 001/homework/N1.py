cfa6102bba4b352ab623bff61c771bf642d13c65
data fofxadze <dfofxadze2010@gmail.com>
2025-01-23 21:01:03 +0400

    commit

--- day 001/homework/N1.py
+++ day 001/homework/N1.py
@@ -1,1 +1,10 @@
-# ვერ გავიგე
+a = 4
+b = 27
+c = 3
+
+MIN = a - (a % c) + c
+
+MAX = b - (b % c)
+
+MAX = MAX - MIN
+print((MAX / c) + 1)
--- day 001/homework/N3.py
+++ day 001/homework/N3.py
@@ -1,1 +1,28 @@
-# ვერც ეს გავიგე (იმედია ახსნის ბატონი ზურაბი <3)
+def finding_max_in_arr(arr):
+
+    maximum = arr[0]
+
+    if arr[1] > maximum:
+        maximum = arr[1]
+    else:
+        maximum = maximum
+    
+    if arr[2] > maximum:
+        maximum = arr[2]
+    else:
+        maximum = maximum
+    
+    if arr[3] > maximum:
+        maximum = arr[3]
+    else:
+        maximum = maximum
+
+    if arr[4] > maximum:
+        maximum = arr[4]
+    else:
+        maximum = maximum
+    
+    return maximum
+    
+arr = [56,19,18,977,2777]
+print(finding_max_in_arr(arr))
